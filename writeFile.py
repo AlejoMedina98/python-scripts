3b0060259c13a9d3cbe6c69fbc64222a8d686db4
Rodrigo Medina <rodrigo.mediancetz@gmail.com>
2019-01-06 22:23:43 -0600

    Reinitialized repo

---
+++ writeFile.py
@@ -0,0 +1,4 @@
+if ( __name__ == '__main__' ):
+    file = open('writeFile.txt', 'w')
+    file.write('Praesentium qui quae est ut. Impedit ut praesentium corrupti et et qui animi. Sed aliquid sint accusamus. Quidem totam qui dolorem molestiae exercitationem. Necessitatibus debitis est beatae est ex. Error et qui deserunt.')
+    file.close()
